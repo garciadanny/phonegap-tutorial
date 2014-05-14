97b5ed4e8c3dd5a1ca5cdbbf213cbf108cd873c4
garciadanny <dannygarcia.me@gmail.com>
2014-05-14 15:13:12 -0600

    create HomeView class to encapsulate the logic to create/render the home view | provide structure

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 var app = {
 
-    findByName: function() {
-      var self = this;
-      this.store.findByName( $('.search-key').val(), function(employees) {
-        $('.employee-list').html(self.employeeLiTpl(employees));
-      });
-    },
-
     showAlert: function(message, title) {
       if (navigator.notification) {
         navigator.notification.alert(message, null, title, 'OK');
@@ -15,20 +8,12 @@ var app = {
       }
     },
 
-    renderHomeView: function() {
-      $('body').html(this.homeTpl());
-      $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-
     initialize: function() {
-      this.homeTpl = Handlebars.compile( $("#home-tpl").html() );
-      this.employeeLiTpl = Handlebars.compile( $("#employee-li-tpl").html() );
       var self = this;
-      this.store = new LocalStorageStore(function(){
-        self.renderHomeView();
-        self.showAlert('Store Initialized', 'Info');
+      this.store = new LocalStorageStore(function() {
+        $('body').html(new HomeView(self.store).render().el);
       });
-      $('.search-key').on('keyup', $.proxy(this.findByName, this));
+      self.showAlert('Store Initialized', 'Info');
     }
 
 };
